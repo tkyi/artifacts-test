af3169b8d73d6b84ced507c9157e7cdace5f244a
Tiffany K <tiffanykyi@gmail.com>
2024-10-22 21:01:39 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 # Create a 1GB file with random content
 import os
-with open("largefile.bin", "wb") as f:
+with open("./artifacts/largefile.bin", "wb") as f:
     f.write(os.urandom(1024 * 1024 * 1024))  # 1GB of random data
 
-with open("largefile2.bin", "wb") as f:
+with open("./artifacts/largefile2.bin", "wb") as f:
     f.write(os.urandom(1024 * 1024 * 1024))  # 1GB of random data
 
-with open("largefile3.bin", "wb") as f:
+with open("./artifacts/largefile3.bin", "wb") as f:
     f.write(os.urandom(1024 * 1024 * 1024))  # 1GB of random data
